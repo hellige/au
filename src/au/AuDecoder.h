82fe4e0a0909937df55c0a46d02646092a9fd336
Matt Godbolt <mgodbolt@drw.com>
2018-03-12 09:23:06 -0500

    Alternative way of CMakeing

---
+++ src/au/AuDecoder.h
@@ -0,0 +1,306 @@
+#pragma once
+
+#include "../JsonHandler.h"
+
+#include <cstdint>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <string_view>
+#include <variant>
+
+#include <fcntl.h>
+#include <unistd.h>
+
+#define THROW(stuff) \
+  do { \
+    std::ostringstream _message; \
+    _message << stuff; \
+    throw parse_error(_message.str()); \
+  } while (0)
+
+
+namespace {
+
+struct parse_error {
+  std::string what;
+
+  parse_error(const std::string &what)
+  : what(what) {}
+};
+
+
+class FileByteSource {
+  static const auto BUFFER_SIZE = 16*1024;
+
+  int fd_;
+  char *cur_;
+  char *limit_;
+  char buf_[BUFFER_SIZE];
+
+public:
+  FileByteSource(const std::string &fname) : cur_(nullptr), limit_(nullptr) {
+    if (fname == "-") {
+      fd_ = fileno(stdin);
+    } else {
+      fd_ = ::open(fname.c_str(), O_RDONLY);
+    }
+    if (fd_ == -1)
+      throw std::runtime_error("open");
+    ::posix_fadvise(fd_, 0, 0, 1);  // FDADVICE_SEQUENTIAL TODO report error?
+    read();
+  }
+
+  ~FileByteSource() {
+    close(fd_); // TODO report error?
+  }
+
+  int next() {
+	while (cur_ == limit_) if (!read()) return EOF;
+    return *cur_++;
+  }
+
+  int peek() {
+    while (cur_ == limit_) if (!read()) return EOF;
+    return *cur_;
+  }
+
+  template <typename T>
+  void read(T *t, size_t len) {
+	char *buf = static_cast<char *>(static_cast<void *>(t));
+    read(len, [&](std::string_view fragment) {
+  	  ::memcpy(buf, fragment.data(), fragment.size());
+	  buf += fragment.size();
+    });
+  }
+
+  template <typename F>
+  void read(size_t len, F func) {
+    while (len) {
+      while (cur_ == limit_)
+        if (!read())
+          THROW("reached eof while trying to read " << len << " bytes");
+      // limit_ > cur_, so cast to size_t is fine...
+      auto first = std::min(len, static_cast<size_t>(limit_ - cur_));
+	  func(std::string_view(cur_, first));
+	  cur_ += first;
+	  len -= first;
+	}
+  }
+
+private:
+  bool read() {
+	cur_ = limit_ = buf_;
+    size_t bytes_read = ::read(fd_, buf_, BUFFER_SIZE);
+    if (bytes_read == (size_t)-1)
+      throw std::runtime_error("read failed");
+    if (!bytes_read) return false;
+    limit_ = buf_ + bytes_read;
+    return true;
+//        for(char *p = buf; (p = (char*) memchr(p, '\n', (buf + bytes_read) - p)); ++p)
+//            ++lines;
+  }
+};
+
+
+template <typename Handler>
+class Parser {
+  static constexpr int AU_FORMAT_VERSION = 1;
+  FileByteSource &source_;
+  Handler &handler_;
+
+public:
+  Parser(FileByteSource &source, Handler &handler)
+  : source_(source), handler_(handler) {}
+
+  void parseStream() const {
+    while (source_.peek() != EOF) record();
+  }
+
+private:
+  void record() const {
+    int c = source_.next();
+    switch (c) {
+    case 'H':
+      if (source_.next() == 'I') {
+    	int64_t version = readVarint();
+    	if (version != AU_FORMAT_VERSION) {
+    	  THROW("Bad format version: expected " << AU_FORMAT_VERSION
+            << ", got " << version);
+        }
+      } else {
+        THROW("Expected version number"); // TODO
+      }
+      term();
+      break;
+    case 'C':
+      term();
+      handler_.onDictClear();
+      break;
+    case 'A': {
+  	  auto backref = readVarint();
+      (void)backref; // TODO
+      handler_.onDictAddStart();
+      while (source_.peek() != 'E') {
+    	  if (source_.next() != 'S')
+    		  THROW("Expected a string"); // TODO
+    	  parseString();
+      }
+      handler_.onDictAddEnd();
+      term();
+      break;
+    }
+    case 'V': {
+      auto backref = readVarint();
+      (void)backref; // TODO
+      auto len = readVarint();
+      (void)len; // TODO
+      value();
+      term();
+      break;
+    }
+    default:
+      THROW("Unexpected character at start of record: '" << (char)c
+    		  << "' (0x" << std::hex << c << ")");
+    }
+    handler_.onRecordEnd();
+  }
+
+  void expect(char e) const {
+    int c = source_.next();
+    if (c == e) return;
+    THROW("Unexpected character: '" << (char)c
+  		  << "' (0x" << std::hex << c << ")");
+  }
+
+  void term() const {
+    expect('E'); expect('\n');
+  }
+
+  double readDouble() const {
+      double val;
+      static_assert(sizeof(val) == 8, "sizeof(double) must be 8");
+      source_.read(&val, sizeof(val));
+      return val;
+  }
+
+  uint64_t readVarint() const {
+    auto shift = 0u;
+    uint64_t result = 0;
+    while (true) {
+      uint64_t i = source_.next();
+      result |= (i & 0x7f) << shift;
+      shift += 7;
+      if (!(i & 0x80)) break;
+    }
+    return result;
+  }
+
+  void value() const {
+	int c = source_.next();
+	switch (c) {
+	case 'T': handler_.onBool(true); break;
+	case 'F': handler_.onBool(false); break;
+	case 'N': handler_.onNull(); break;
+	case 'I': handler_.onUint(readVarint()); break;
+	case 'J': handler_.onInt(-readVarint()); break;
+	case 'D': handler_.onDouble(readDouble()); break;
+	case 'X': handler_.onDictRef(readVarint()); break;
+	case 'S': parseString(); break;
+	case '[': parseArray(); break;
+	case '{': parseObject(); break;
+	default:
+      THROW("Unexpected character at start of value: '" << (char)c
+        << "' (0x" << std::hex << c << ")");
+	}
+  }
+
+  void parseString() const {
+    auto len = readVarint();
+    handler_.onStringStart(len);
+    source_.read(len, [&](std::string_view fragment) {
+      handler_.onStringFragment(fragment);
+    });
+    handler_.onStringEnd();
+  }
+
+  void parseArray() const {
+	handler_.onArrayStart();
+	while (source_.peek() != ']') value();
+	expect(']');
+	handler_.onArrayEnd();
+  }
+
+  void parseObject() const {
+	handler_.onObjectStart();
+	while (source_.peek() != '}') {
+		value();
+		value();
+	}
+	expect('}');
+	handler_.onObjectEnd();
+  }
+};
+
+}
+
+struct NoopHandler {
+  void onRecordEnd() {}
+  void onObjectStart() {}
+  void onObjectEnd() {}
+  void onArrayStart() {}
+  void onArrayEnd() {}
+  void onNull() {}
+  void onBool(bool) {}
+  void onInt(int64_t) {}
+  void onUint(uint64_t) {}
+  void onDouble(double) {}
+  void onDictRef(size_t) {}
+  void onDictClear() {}
+  void onDictAddStart() {}
+  void onDictAddEnd() {}
+  void onStringStart(size_t) {}
+  void onStringEnd() {}
+  void onStringFragment(std::string_view) {}
+};
+
+class AuDecoder {
+  std::string filename_;
+
+public:
+  AuDecoder(const std::string &filename)
+  : filename_(filename) {}
+
+  void decode() const {
+    FileByteSource source(filename_);
+    JsonHandler handler;
+    try {
+      Parser(source, handler).parseStream();
+    } catch (parse_error &e) {
+      std::cout << e.what << std::endl;
+    }
+  }
+
+  void decodeNoop() const {
+    FileByteSource source(filename_);
+    NoopHandler handler;
+    try {
+      Parser(source, handler).parseStream();
+    } catch (parse_error &e) {
+      std::cout << e.what << std::endl;
+    }
+  }
+
+  template <typename H>
+  void decode(H &handler) const {
+    FileByteSource source(filename_);
+    try {
+      Parser(source, handler).parseStream();
+    } catch (parse_error &e) {
+      std::cout << e.what << std::endl;
+    }
+  }
+};
---
+++ test/AuUnitTests.cpp
@@ -0,0 +1,222 @@
+#include "AuEncoder.h"
+
+#include "gtest/gtest.h"
+
+#include <cmath>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+TEST(AuStringIntern, NoIntern) {
+  AuStringIntern si;
+  EXPECT_EQ(0, si.dict().size());
+  EXPECT_FALSE(si.idx(std::string("shrt"), std::optional<bool>()));
+  EXPECT_FALSE(si.idx(std::string("Long string"), std::optional<bool>()));
+  EXPECT_EQ(0, si.dict().size());
+}
+
+TEST(AuStringIntern, ForceIntern) {
+  AuStringIntern si;
+  EXPECT_EQ(0, si.dict().size());
+
+  // Tiny strings are not interned even if forced
+  EXPECT_FALSE(si.idx(std::string("tiny"), true));
+  EXPECT_EQ(0, si.dict().size());
+
+  EXPECT_TRUE(si.idx(std::string("A normal string"), true));
+  EXPECT_EQ(1, si.dict().size());
+}
+
+TEST(AuStringIntern, InternFrequentStrings) {
+  constexpr size_t INTERN_THRESH = 10;
+  AuStringIntern si(4, INTERN_THRESH, 1000);
+  std::string str("Normal value");
+
+  EXPECT_FALSE(si.idx(str, std::optional<bool>()));
+  EXPECT_EQ(0, si.dict().size());
+
+  for (size_t i = 0; i < INTERN_THRESH * 2 && !HasFailure(); ++i) {
+    if (i < INTERN_THRESH - 1) {
+      EXPECT_FALSE(si.idx(str, std::optional<bool>())) << "i = " << i;
+      EXPECT_EQ(0, si.dict().size()) << "i = " << i;
+    } else {
+      EXPECT_TRUE(si.idx(str, std::optional<bool>()));
+      EXPECT_EQ(1, si.dict().size());
+    }
+  }
+}
+
+struct AuFormatterTest : public ::testing::Test {
+  Au::VectorBuffer buf;
+  AuStringIntern stringIntern;
+  AuFormatter<Au::VectorBuffer> formatter;
+
+  AuFormatterTest() : formatter(buf, stringIntern) {}
+};
+
+TEST_F(AuFormatterTest, Null) {
+  formatter.null();
+  formatter.value(nullptr);
+  EXPECT_EQ(std::string("NN"), buf.str());
+}
+
+TEST_F(AuFormatterTest, Bool) {
+  formatter.value(true);
+  formatter.value(false);
+  EXPECT_EQ(std::string("TF"), buf.str());
+}
+
+#define C(v) static_cast<char>(v)
+
+TEST_F(AuFormatterTest, Int) {
+  formatter.value(0).value(127).value(128);
+  formatter.value(-1).value(-127).value(-128);
+  formatter.value(0xff).value(0x100);
+
+  std::vector<char> ints = {
+      // Small positives
+      'I', 0,             // 0
+      'I', C(127),        // 127
+      'I', C(0x80), 0x01, // 128
+      // Small negatives
+      'J', 1,             // -1
+      'J', C(127),        // -127
+      'J', C(0x80), 0x01, // -128
+      // Larger positives
+      'I', C(0xff), 0x01, // 0xff
+      'I', C(0x80), 0x02, // 0x100
+  };
+  EXPECT_EQ(std::string(ints.data(), ints.size()), buf.str());
+}
+
+TEST_F(AuFormatterTest, Int64) {
+  formatter.value(0x1234567890abcdef);
+  formatter.value(-0x1234567890abcdef);
+  formatter.value(0xf234567890abcdef);
+  formatter.value(0xffffFFFFffffFFFF);
+  std::vector<char> ints = {
+      'I', C(0xef), C(0x9b), C(0xaf), C(0x85), C(0x89), C(0xcf), C(0x95),
+      C(0x9a), C(0x12),
+      'J', C(0xef), C(0x9b), C(0xaf), C(0x85), C(0x89), C(0xcf), C(0x95),
+      C(0x9a), C(0x12),
+      'I', C(0xef), C(0x9b), C(0xaf), C(0x85), C(0x89), C(0xcf), C(0x95),
+      C(0x9a), C(0xf2), C(0x01),
+      'I', C(0xff), C(0xff), C(0xff), C(0xff), C(0xff), C(0xff), C(0xff),
+      C(0xff), C(0xff), C(0x01)
+  };
+  EXPECT_EQ(std::string(ints.data(), ints.size()), buf.str());
+}
+
+TEST_F(AuFormatterTest, Double) {
+  double d = 5.9;
+  formatter.value(d);
+  EXPECT_EQ(std::string("D\x9A\x99\x99\x99\x99\x99\x17\x40"), buf.str());
+}
+
+TEST_F(AuFormatterTest, Float) {
+  float f = 5.9;
+  formatter.value(f);
+  EXPECT_EQ(std::string("D\0\0\0\xA0\x99\x99\x17\x40", 9), buf.str());
+}
+
+TEST_F(AuFormatterTest, NaN) {
+  formatter.array(
+      std::numeric_limits<float>::quiet_NaN(),
+      std::numeric_limits<double>::quiet_NaN(),
+      std::numeric_limits<long double>::quiet_NaN(), // converted to double
+      0 / 0.0,
+      std::sqrt(-1)
+  );
+
+  std::vector<char> NaNs = { '[',
+                             'D', 0, 0, 0, 0, 0, 0, C(0xf8), C(0x7f),
+                             'D', 0, 0, 0, 0, 0, 0, C(0xf8), C(0x7f),
+                             'D', 0, 0, 0, 0, 0, 0, C(0xf8), C(0x7f),
+                             'D', 0, 0, 0, 0, 0, 0, C(0xf8), C(0xff),
+                             'D', 0, 0, 0, 0, 0, 0, C(0xf8), C(0xff),
+                             ']'
+  };
+  EXPECT_EQ(std::string(NaNs.data(), NaNs.size()), buf.str());
+}
+
+TEST_F(AuFormatterTest, Inf) {
+  formatter.array(
+      std::numeric_limits<double>::infinity(),
+      -std::numeric_limits<double>::infinity(),
+      std::numeric_limits<float>::infinity(),
+      -std::numeric_limits<float>::infinity()
+  );
+
+  std::vector<char> Infs = { '[',
+                             'D', 0, 0, 0, 0, 0, 0, C(0xf0), C(0x7f),
+                             'D', 0, 0, 0, 0, 0, 0, C(0xf0), C(0xff),
+                             'D', 0, 0, 0, 0, 0, 0, C(0xf0), C(0x7f),
+                             'D', 0, 0, 0, 0, 0, 0, C(0xf0), C(0xff),
+                             ']'
+  };
+  EXPECT_EQ(std::string(Infs.data(), Infs.size()), buf.str());
+}
+
+TEST_F(AuFormatterTest, ShortString) {
+  formatter.value("str");
+  std::vector<char> str = {'S', 0x03, 's', 't', 'r'};
+  EXPECT_EQ(std::string(str.data(), str.size()), buf.str());
+}
+
+TEST_F(AuFormatterTest, LongString) {
+  formatter.value("aLongerString");
+  std::vector<char> str = {'S', 13, 'a', 'L', 'o', 'n', 'g', 'e', 'r', 'S', 't',
+                           'r', 'i', 'n', 'g'};
+  EXPECT_EQ(std::string(str.data(), str.size()), buf.str());
+}
+
+TEST_F(AuFormatterTest, InternString) {
+  stringIntern.idx(std::string("aLongInternedString"), true);
+  stringIntern.idx(std::string("another string"), true);
+
+  formatter.value("aLongInternedString", true);
+  formatter.value("another string", true);
+
+  EXPECT_EQ(std::string("X\0X\1", 4), buf.str());
+}
+
+TEST_F(AuFormatterTest, EmptyMap) {
+  formatter.map();
+  EXPECT_EQ(std::string("{}"), buf.str());
+}
+
+TEST_F(AuFormatterTest, FlatMap) {
+  formatter.map("Key1", "value1", "key1", "Value1");
+  EXPECT_EQ(std::string("{S\x04Key1S\x06value1S\04key1S\x06Value1}"), buf.str());
+}
+
+TEST_F(AuFormatterTest, NestedMap) {
+  formatter.map("k1", "v1", "nested", formatter.mapVals([&](auto &sink) {
+    sink(std::string_view("k2"), "v2");
+  }));
+  EXPECT_EQ(std::string("{S\2k1S\2v1X\0{S\2k2S\2v2}}", 22), buf.str());
+}
+
+TEST_F(AuFormatterTest, EmptyArray) {
+  formatter.array();
+  EXPECT_EQ(std::string("[]"), buf.str());
+}
+
+TEST_F(AuFormatterTest, FlatArray) {
+  formatter.array(1, 2, 3);
+  EXPECT_EQ(std::string("[I\1I\2I\3]"), buf.str());
+}
+
+TEST_F(AuFormatterTest, NestedArray) {
+  formatter.array(1, 2, formatter.arrayVals([&]() {
+    formatter.value(3).value(4);
+  }));
+  EXPECT_EQ(std::string("[I\1I\2[I\3I\4]]"), buf.str());
+}
+
+TEST(Au, creation) {
+  std::ostringstream os;
+  Au au(os);
+}
+
