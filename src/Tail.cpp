5a6a25522d47161e9ea55ec0ae30cc5b1c849df8
Gabriel Burca <gburca@drw.com>
2018-03-14 09:16:31 -0500

    Tail stub

--- src/Tail.cpp
+++ src/Tail.cpp
@@ -5,8 +5,22 @@
 
 #include "JsonHandler.h"
 
+template<typename OutputHandler>
+class TailHandler : public NoopValueHandler {
+  OutputHandler &outputHandler_;
+  Dictionary &dictionary_;
+
+public:
+  TailHandler(Dictionary &dictionary, OutputHandler &handler)
+      : outputHandler_(handler), dictionary_(dictionary)
+  {}
+
+  void onValue(FileByteSource &) {}
+};
+
 int tail(int argc, char **argv) {
   Dictionary dictionary;
+  JsonHandler jsonHandler(dictionary);
 
   try {
     TCLAP::CmdLine cmd("Tail sub-command", ' ', AU_VERSION, true);
@@ -16,8 +30,20 @@ int tail(int argc, char **argv) {
                             cmd, false);
     TCLAP::ValueArg lines("n", "lines", "output last n lines (default 10)",
                           false, 10, "integer", cmd);
-
+    TCLAP::UnlabeledMultiArg<std::string> fileNames("fileNames", "Au files",
+                                                    false, "FileName", cmd);
     cmd.parse(argc, argv);
+
+    TailHandler<JsonHandler> tailHandler(dictionary, jsonHandler);
+    RecordHandler<decltype(tailHandler)> recordHandler(dictionary, tailHandler);
+
+    if (fileNames.getValue().empty()) {
+      std::cerr << "Tailing stdin\n";
+      AuDecoder("-").decode(recordHandler);
+    } else {
+      // TODO: Handle multiple files
+      AuDecoder(fileNames.getValue()[0]).decode(recordHandler);
+    }
 
   } catch (TCLAP::ArgException &e) {
     std::cerr << "error: " << e.error() << " for arg " << e.argId() << std::endl;
