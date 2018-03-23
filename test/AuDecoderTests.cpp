cb758eb82d9d6a96f274b0606ce95284ce2281eb
Gabriel Burca <gburca@drw.com>
2018-03-23 10:22:24 -0500

    Test tweak

--- test/AuDecoderTests.cpp
+++ test/AuDecoderTests.cpp
@@ -5,6 +5,6 @@
 TEST(JsonOutputHandler, Time) {
   using namespace std::chrono;
   JsonOutputHandler json;
-  json.onTime(0, nanoseconds(1'000'000));
-  EXPECT_EQ(json.str(), R"("1970-01-01T00:00:00.001000")");
+  json.onTime(0, nanoseconds(123'456'789));
+  EXPECT_EQ(json.str(), R"("1970-01-01T00:00:00.123456")");
 }
