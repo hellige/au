5310200b2efaa612128073bc91c37e8a87b56a7b
Gabriel Burca <gburca@drw.com>
2018-03-23 10:21:57 -0500

    Let git do the cloning
    - Add custom .gitconfig for git submodules
    - Make the manual git cloning recursive

--- drw/clone.sh
+++ drw/clone.sh
@@ -3,25 +3,38 @@
 # Clones submodules from git.drwholdings.com:mirror instead of github to bypass
 # issues with cloning from https URLs on old build servers.
 
-# TODO: Handle recursive cloning of submodules
-
 set -x
+ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+SCRIPT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")"
 
-clone() {
+manualClone() {
     local DIR=$(echo "$1" | sed 's/path = \(.*\)/\1/')
     local URL=$(echo "$2" | sed 's/url = \(.*\)/\1/')
     #local REPO=$(basename --suffix=.git "$URL")
     local REPO=$(basename "$URL")
+    REPO=${REPO%.git}
     local NEW_URL="git@git.drwholdings.com:mirror/${REPO}.git"
 
     echo "DIR = $DIR   URL = $URL   NEW_URL = $NEW_URL"
     mkdir -p $DIR
-    cd $DIR
-    git clone "$NEW_URL" .
-    git fetch origin
-    cd -
+    pushd $DIR
+    if [ -z "$(ls -A .)" ]; then
+        git clone "$NEW_URL" .
+    else
+        git fetch origin
+    fi
+    if [ -f .gitmodules ]; then
+        $(${SCRIPT})
+    fi
+    popd
 }
 
-while read SUBMOD; read DIR; read URL; do
-    clone "$DIR" "$URL"
-done
+parseGitModules() {
+    cat .gitmodules | while read SUBMOD; read DIR; read URL; do
+        manualClone "$DIR" "$URL"
+    done
+}
+
+#parseGitModules
+env HOME=${ROOT_DIR}/drw git submodule init
+env HOME=${ROOT_DIR}/drw git submodule update --force
