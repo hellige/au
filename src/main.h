48f9a3b1d6b79a4a71977fedc49b2960382430a7
Matt Hellige <mhellige@drw.com>
2018-11-21 16:32:25 -0600

    Bump version.

--- src/main.h
+++ src/main.h
@@ -1,6 +1,6 @@
 #pragma once
 
-constexpr const char *AU_VERSION = "0.2.1";
+constexpr const char *AU_VERSION = "0.2.2";
 
 int json2au(int argc, const char * const *argv);
 int stats(int argc, const char * const *argv);
